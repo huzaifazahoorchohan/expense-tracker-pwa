c221bec3514f9e80aafe9b231947ea9c5151c174
Huzaifa Zahoor <57295548+huzaifazahoorchohan@users.noreply.github.com>
2020-11-11 03:06:52 +0500

    Create service worker

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+let CACHE_NAME = "website";
+let urlsToCache = [
+  "/static/js/bundle.ts",
+  "/static/js/main.chunk.ts",
+  "/static/js/1.chunk.ts",
+  "index.html",
+  "/",
+];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
--- src/State/GlobalContext.tsx
+++ src/State/GlobalContext.tsx
@@ -25,27 +25,6 @@ export const TransactionProvide: React.FC = ({ children }) => {
         })
     }
 
-    let getIncome = () => {
-        let income: number = 0;
-        for (let index = 0; index < state.transaction.length; index++) {
-            if (state.transaction[index].amount > 0) {
-                income = income + state.transaction[index].amount;
-            }
-
-        }
-        return income;
-    }
-    let getExpense = () => {
-        let expense: number = 0;
-        for (let index = 0; index < state.transaction.length; index++) {
-            if (state.transaction[index].amount < 0) {
-                expense = expense + state.transaction[index].amount;
-            }
-
-        }
-        return expense;
-    }
-
     return (
         <TransactionContext.Provider value={{
             transaction: state.transaction,
